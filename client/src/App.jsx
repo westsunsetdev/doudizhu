793f3dffd334731d4e76d4bf57679991fa2eca01
Jeffrey Kim <jeffreykim@jeffreys-mbp.mynetworksettings.com>
2025-07-31 21:07:09 -0400

    refactor for local machine dev

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,9 +2,14 @@ import React, { useEffect, useState } from "react";
 import io from "socket.io-client";
 import "./App.css";
 
-// const socket = io( 'https://5838ae2e-a904-45d9-b4ea-0cfac1ad43bb-00-12jnvqrv7ilzh.kirk.replit.dev/',
-  // `${window.location.protocol}//${window.location.hostname}:3001`,
-const socket = io( `${window.location.protocol}//${window.location.hostname}:3001`,
+const socket = io( 
+  // Comment out the version you're not using.
+  
+  // Replit version:
+  // 'https://5838ae2e-a904-45d9-b4ea-0cfac1ad43bb-00-12jnvqrv7ilzh.kirk.replit.dev/',
+  
+  // Cursor / local version:
+  `${window.location.protocol}//${window.location.hostname}:3001`,
   {
     transports: ["websocket", "polling"],
     forceNew: true,
